aa94799986596ec7a65560c8897068bfcfd7214f
gtamie <89866151+gtamie@users.noreply.github.com>
2021-11-18 14:20:44 -0300

    Adicionei o arquivo csv para armazenar os dados dos pacientes. ATENÇÃO: está havendo erro na hora de inserir os pacientes. Está criando no csv uma linha em branco.

--- cadastroPacientes/cadastroPacientes.py
+++ cadastroPacientes/cadastroPacientes.py
@@ -1,12 +1,12 @@
 from funcoesCadastroPacientes import *
 pacientes={}
-
 prontuario=[]
 print("SISTEMA DE CADASTRO DE PACIENTES")
 opcao=apresentarMenu()
+
 while opcao=="I" or opcao=="P"or opcao=="E" or opcao=="L":
     if opcao=="I":
-       inserirPaciente(pacientes,prontuario)
+        inserirPaciente(pacientes,prontuario)
     if opcao=="P":
         pesquisarPaciente(pacientes,int(input("Digite o número do prontuário: ")))
     if opcao=="E":
--- cadastroPacientes/funcoesCadastroPacientes.py
+++ cadastroPacientes/funcoesCadastroPacientes.py
@@ -7,16 +7,41 @@ def apresentarMenu():
                "\nInforme a opção desejada:").upper()
     return menu
 
+
 def inserirPaciente(dicionario,listaprontuario):
+    # inserir dados do "fichapacientes.csv" para o dicionário (no caso de já existir pacientes cadastrados no arquivo)
+    with open("fichapacientes.csv", "r") as ficha:
+        conteudo = ficha.readlines()
+        if len(conteudo) > 1:
+            with open("fichapacientes.csv", "r") as ficha:
+                dados = ficha.readlines()[1:]
+                for linha in dados:
+                    lista=linha.split(";")
+                    number=int(lista[0])
+                    listaprontuario.append(number)
+                    nome=lista[1]
+                    dtnasc=lista[2]
+                    convenio=lista[3]
+                    dicionario[number] = [nome, dtnasc, convenio]
+
     # gerar o número de prontuario
     if len(dicionario) == 0:
         number = 1
     else:
         number = listaprontuario[-1] + 1
     listaprontuario.append(number)
-    # inserir dados dod paciente
-    dicionario[number]=[input("Nome completo: "), input("Data de nascimento:"), input("Convênio:")]
+    # inserir dados do paciente no dicionário
+    nome=input("Nome completo: ").upper()
+    dtnasc=input("Data de nascimento:")
+    convenio=input("Convênio:").upper()
+    dicionario[number]=[nome, dtnasc, convenio]
 
+    # guardar dados no arquivo csv
+    with open("fichapacientes.csv", "w") as ficha:
+        ficha.write("Nº prontuário" + ";" + "Nome completo" + ";" + "Data nascimento" + ";" + "Convênio"+"\n")
+    with open("fichapacientes.csv", "a") as ficha:
+        for chave,valor in dicionario.items():
+            ficha.write(str(chave)+ ";" + valor[0] + ";" + valor[1] + ";" + valor[2]+"\n")
 
 
 def pesquisarPaciente(dicionario,chave):
@@ -32,6 +57,7 @@ def excluirPaciente(dicionario,chave):
         print("Cadastro excluído.")
 
 def listarPacientes(dicionario):
+
     for chave,valor in dicionario.items():
         print("Nome: ",valor[0])
         print("Prontuário: ", chave)
